c57f2d2db89692ba717180c2d56ecb7e999125b1
Alicia Moses <aliciamoses.dev@gmail.com>
2023-04-16 23:59:28 +0100

    Added model Component file

---
+++ src/components/deletePostModal/deletePostModal.tsx
@@ -0,0 +1,47 @@
+const deletePostModal = ()  => {
+    
+    return (
+        <>
+          <div className="fixed z-10 inset-0 overflow-y-auto">
+    <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+      <div className="fixed inset-0 transition-opacity" aria-hidden="true">
+        <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+      </div>
+      <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+        &#8203;
+      </span>
+      <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+        <div>
+          <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-title">
+            Confirm Post Deletion
+          </h3>
+          <div className="mt-2">
+            <p className="text-sm text-gray-500">
+              Are you sure you want to delete this post? This action cannot be undone.
+            </p>
+          </div>
+        </div>
+        <div className="mt-5 sm:mt-6">
+          <button
+            type="button"
+            onClick={confirmDelete}
+            className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm"
+          >
+            Delete
+          </button>
+          <button
+            type="button"
+            onClick={toggleDeleteModal}
+            className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+          >
+            Cancel
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+        </>
+    );
+  };
+
+  export default deletePostModal;
