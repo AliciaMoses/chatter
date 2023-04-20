890832d2807a5159983e501a0236db6297b6a0fb
Alicia Moses <aliciamoses.dev@gmail.com>
2023-04-20 21:11:50 +0100

    Added font family to globals.css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Courier New', 'monospace'],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
