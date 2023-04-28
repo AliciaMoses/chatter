ce197e598ec868c5e7ef378336493d44e64a2894
Alicia Moses <aliciamoses.dev@gmail.com>
2023-04-28 16:42:26 +0100

    Updated LoadingPosts animation: images now rendering with animation before loading 404 if no data found

--- src/components/loadingPosts/loadingPosts.tsx
+++ src/components/loadingPosts/loadingPosts.tsx
@@ -2,38 +2,27 @@ import Image from "next/image";
 
 const LoadingPosts = () => {
   return (
-    <div role="status">
-      <Image
-        src="/images/aliens/1.png"
-        alt="Loading..."
-        width={64}
-        height={64}
-        className="text-primary inline-block animate-[spinner-grow_0.75s_linear_infinite] rounded-full align-[-0.125em] opacity-0 motion-reduce:animate-[spinner-grow_1.5s_linear_infinite]"
-      />
-      <Image
-        src="/images/aliens/2.png"
-        alt="Loading..."
-        width={64}
-        height={64}
-        className="text-secondary inline-block animate-[spinner-grow_0.75s_linear_infinite] rounded-full align-[-0.125em] opacity-0 motion-reduce:animate-[spinner-grow_1.5s_linear_infinite]"
-      />
-      <Image
-        src="/images/aliens/3.png"
-        alt="Loading..."
-        width={64}
-        height={64}
-        className="text-success inline-block animate-[spinner-grow_0.75s_linear_infinite] rounded-full align-[-0.125em] opacity-0 motion-reduce:animate-[spinner-grow_1.5s_linear_infinite]"
-      />
-      <Image
-        src="/images/aliens/4.png"
-        alt="Loading..."
-        width={64}
-        height={64}
-        className="text-danger inline-block animate-[spinner-grow_0.75s_linear_infinite] rounded-full align-[-0.125em] opacity-0 motion-reduce:animate-[spinner-grow_1.5s_linear_infinite]"
-      />
-
-      <span className="sr-only">Loading...</span>
-    </div>
+    <>
+      <div
+        className="flex items-center justify-center min-h-screen"
+        role="status"
+      >
+        <div className="space-x-4">
+          <div className="inline-block h-12 w-12 animate-bounce motion-reduce:animate-[bounce_1.5s_linear_infinite]">
+            <Image src="/images/aliens/1.png" alt="Loading..." width={64} height={64} />
+          </div>
+          <div className="inline-block h-12 w-12 animate-bounce motion-reduce:animate-[bounce_1.5s_linear_infinite]">
+            <Image src="/images/aliens/2.png" alt="Loading..." width={64} height={64} />
+          </div>
+          <div className="inline-block h-12 w-12 animate-bounce motion-reduce:animate-[bounce_1.5s_linear_infinite]">
+            <Image src="/images/aliens/3.png" alt="Loading..." width={64} height={64} />
+          </div>
+          <div className="inline-block h-12 w-12 animate-bounce motion-reduce:animate-[bounce_1.5s_linear_infinite]">
+            <Image src="/images/aliens/4.png" alt="Loading..." width={64} height={64} />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
