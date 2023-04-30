3e3e9b62979347dc4a4e7f05c9c4e7e57bee7082
Alicia Moses <aliciamoses.dev@gmail.com>
2023-04-30 15:04:51 +0100

    Updated svg property names

--- src/components/deletePostModal/deletePostModal.tsx
+++ src/components/deletePostModal/deletePostModal.tsx
@@ -35,9 +35,9 @@ const DeletePostModal = ({
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               >
                 {" "}
                 <circle cx="12" cy="12" r="10" />{" "}
