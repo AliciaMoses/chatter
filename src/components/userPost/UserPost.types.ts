6b5e678a9d6952e2329383a210d74ee70f61fd6c
Alicia Moses <aliciamoses.dev@gmail.com>
2023-05-03 20:48:55 +0100

    Updated UserPost props to include replies

--- src/components/userPost/UserPost.types.ts
+++ src/components/userPost/UserPost.types.ts
@@ -1,7 +1,20 @@
 import { type RouterOutputs } from "~/utils/api";
 
-export type IndividualPost = RouterOutputs["posts"]["getAll"][number];
+
+export type IndividualPost = RouterOutputs["posts"]["getAllWithReplies"][number];
 
 export type UserPostProps = IndividualPost & {
+  onPostDeleted?: () => void;
+  parentPostId?: string;
+};
+
+export type UserPostWithData = {
+  post: IndividualPost;
+  author: {
+    username: string;
+    id: string;
+    profileImageUrl: string;
+    externalUsername: string | null;
+  };
   onPostDeleted?: () => void;
 };
