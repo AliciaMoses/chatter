a6a93d324b90b6c18dfecac8a2b51ee45d05e5b4
Alicia Moses <aliciamoses.dev@gmail.com>
2023-05-04 23:32:18 +0100

    Created Post Container Created functional component for loading posts

--- src/components/loadingPosts/loadingPosts.tsx
+++ src/components/loadingPosts/loadingPosts.tsx
@@ -1,6 +1,7 @@
+import React from 'react';
 import Image from "next/image";
 
-const LoadingPosts = () => {
+const LoadingPosts: React.FC = () => {
   return (
     <>
       <div
---
+++ src/components/postContainer/PostContainer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PostContainer: React.FC = () => {
+  return <div className="postContainer flex-grow"></div>;
+};
+
+export default PostContainer;
