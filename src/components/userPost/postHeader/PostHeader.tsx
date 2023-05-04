6f73661939f406f87ad456f3222ccaa82014a1cb
Alicia Moses <aliciamoses.dev@gmail.com>
2023-05-04 23:19:11 +0100

    Refactored Post Header

--- src/components/userPost/postHeader/PostHeader.tsx
+++ src/components/userPost/postHeader/PostHeader.tsx
@@ -4,28 +4,25 @@ import Image from "next/image";
 
 import { type UserPostProps } from "../UserPost.types";
 
-
-const PostHeader = (props: UserPostProps) => {
-
-  const { author } = props;  
+const PostHeader: React.FC<UserPostProps> = ({ author }) => {
   return (
-    <> 
-          <div className="postHeader">
-            <div className="postAvatar">
-              <Link href={`/profile/@${author.username}`}>
-                <Image
-                  src={author.profileImageUrl}
-                  alt="Author profile image"
-                  width={64}
-                  height={64}
-                  className="rounded-full object-cover shadow-md shadow-violet-700 ring-2 ring-violet-800 ring-offset-2"
-                />
-              </Link>
-            </div>
-            <Link href={`/profile/@${author.username}`}>
-              <div className="postUsername">@{author.username}</div>
-            </Link>
-            </div>
+    <>
+      <div className="postHeader">
+        <div className="postAvatar">
+          <Link href={`/profile/@${author.username}`}>
+            <Image
+              src={author.profileImageUrl}
+              alt="Author profile image"
+              width={64}
+              height={64}
+              className="rounded-full object-cover shadow-md shadow-violet-700 ring-2 ring-violet-800 ring-offset-2"
+            />
+          </Link>
+        </div>
+        <Link href={`/profile/@${author.username}`}>
+          <div className="postUsername">@{author.username}</div>
+        </Link>
+      </div>
     </>
   );
 };
