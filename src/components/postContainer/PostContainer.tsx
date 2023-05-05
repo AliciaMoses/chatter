9ba71e98928261ae765ed3899968d57c212c05e8
Alicia Moses <aliciamoses.dev@gmail.com>
2023-05-05 23:00:11 +0100

    Add PostContainer for UserPost and replies

--- src/components/postContainer/PostContainer.tsx
+++ src/components/postContainer/PostContainer.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import RepliesContainer from "../repliesContainer/RepliesContainer";
+import UserPost from "../userPost/UserPost";
+import { type UserPostProps } from "../userPost/UserPost.types";
+import CreateReplyContainer from "../createReplyContainer/CreateReplyContainer";
 
-const PostContainer: React.FC = () => {
-  return <div className="postContainer flex-grow"></div>;
+const PostContainer: React.FC<UserPostProps> = ({ post, author, onPostDeleted }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+
+  const onNewPostCreated = () => {
+    setIsExpanded(false);
+
+  };
+
+
+  return (
+    <div className="postContainer flex-grow">
+      <UserPost post={post} author={author} onPostDeleted={onPostDeleted} />
+  
+      <CreateReplyContainer isExpanded={isExpanded} parentPostId={post.id} onNewPostCreated={onNewPostCreated} />
+      <RepliesContainer parentPostId={post.id} />
+    </div>
+  );
 };
 
 export default PostContainer;
