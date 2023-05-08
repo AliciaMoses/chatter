7cbf3620bed24badd14daef030d0cf665b308fde
Alicia Moses <aliciamoses.dev@gmail.com>
2023-05-08 21:57:16 +0100

    Updated StartTour lead text styling

--- src/components/landingPage/startTour/StartTour.tsx
+++ src/components/landingPage/startTour/StartTour.tsx
@@ -5,10 +5,10 @@ const StartTour: React.FC = () => {
     <>
       <section className="relative isolate overflow-hidden bg-white px-6 py-12 sm:py-16 lg:px-8">
         <div className="my-4 flex justify-center">
-          <p className="font-vt323 text-5xl font-black text-violet-900">Reimagining social media for developers</p>
+          <p className="text-shadow-3d font-vt323 text-5xl text-slate-900 ;">Reimagining social media for developers</p>
         </div>
         <div className="my-4 flex justify-center">
-          <p className="font-vt323 text-3xl text-slate-900 ">Take a Tour</p>
+          <p className="font-vt323 text-3xl text-violet-900 ">Take a Tour</p>
         </div>
       </section>
     </>
