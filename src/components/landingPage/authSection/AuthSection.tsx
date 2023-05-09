371782595a21061e0c44d284c7f39b8eb0f41660
Alicia Moses <aliciamoses.dev@gmail.com>
2023-05-09 22:12:22 +0100

    Added Clerk Auth buttons

--- src/components/landingPage/authSection/AuthSection.tsx
+++ src/components/landingPage/authSection/AuthSection.tsx
@@ -1,3 +1,4 @@
+import { SignUpButton, SignInButton } from "@clerk/nextjs";
 import React from "react";
 import GitHubIcon from "./GitHubIcon";
 import PrintIcon from "./PrintIcon";
@@ -22,18 +23,22 @@ const AuthSection: React.FC = () => {
               Sign in using your GitHub account and get started instantly...{" "}
             </p>
             <div className="mt-6 flex max-w-md gap-x-4">
+              <SignUpButton mode="modal" afterSignInUrl="/feed">
               <button
                 type="submit"
                 className="flex-none rounded border-2  border-b-4 border-violet-800 bg-violet-500 px-4 py-2 text-sm font-bold text-violet-950 shadow-md shadow-violet-300 hover:border-violet-800 hover:bg-violet-400 focus:outline-none focus:ring-4 focus:ring-violet-300 focus-visible:outline focus-visible:outline-2   focus-visible:outline-offset-2 focus-visible:outline-violet-500 dark:bg-violet-100 dark:hover:bg-violet-200 dark:focus:ring-violet-800"
               >
                 Sign Up
               </button>
+              </SignUpButton>
+              <SignInButton mode="modal" afterSignInUrl="/feed">
               <button
                 type="submit"
                 className="flex-none rounded border-2  border-b-4 border-violet-800 bg-white px-4 py-2 text-sm  text-violet-950 shadow-md shadow-violet-300 hover:border-violet-600 hover:bg-violet-200 focus:outline-none focus:ring-4 focus:ring-violet-300 focus-visible:outline focus-visible:outline-2   focus-visible:outline-offset-2 focus-visible:outline-violet-500 dark:bg-white dark:hover:bg-violet-200 dark:focus:ring-violet-600"
               >
                 Sign In
               </button>
+              </SignInButton>
             </div>
           </div>
           <dl className="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2">
